f71808eb7acf430268cb2e69abb9b87ba7e6bd8d
putri <putrisanjiwanidewi@gmail.com>
2021-12-30 23:21:45 +0900

    import form an todos

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Todos from './components/Todos';
+import Form from './components/form';
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <Todos/>
+      <Form/>
+    </div>
+    
+    
+  );
+}
+
+export default App;
